28fcb49e72bf84435cc5ed270f17ab7a42145a5a
SagarSharma950 <156985687+SagarSharma950@users.noreply.github.com>
2024-08-14 10:22:17 +0530

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,12 @@
+class Data
+{
+	public static void main(String args[]){
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter a and b: ");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		System.out.println("The result is: ");
+		System.out.println(a+b);
+		
+	}
+}
