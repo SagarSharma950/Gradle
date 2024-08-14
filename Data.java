dd0953a57cb35531ca06921c8d559124eb721c44
SagarSharma950 <156985687+SagarSharma950@users.noreply.github.com>
2024-08-14 10:56:32 +0530

    Update Data.java

--- Data.java
+++ Data.java
@@ -1,12 +1,7 @@
 class Data
 {
 	public static void main(String args[]){
-		Scanner sc= new Scanner(System.in);
-		System.out.println("Enter a and b: ");
-		int a=sc.nextInt();
-		int b=sc.nextInt();
-		System.out.println("The result is: ");
-		System.out.println(a+b);
+		System.out.println("helloworld");
 		
 	}
 }
